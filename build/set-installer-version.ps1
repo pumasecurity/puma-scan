1c25f891de6b8c5e06ca3da4328c20bc68f47628
Eric M Mead <eric.mead@pumasecurity.io>
2022-01-31 22:08:57 -0600

    update build scripts to include 2022 version

--- build/set-installer-version.ps1
+++ build/set-installer-version.ps1
@@ -20,4 +20,21 @@ $node.SetAttribute("Version", $BuildVersion);
 Write-Host "Saving $vsixManifestPath..."
 $xml.Save($vsixManifestPath)
 
+# vsixmanifest 2022
+$vsixManifestPath = Get-Location | Resolve-Path | Join-Path -ChildPath ".\Puma.Security.Rules.Vsix.VS2022\source.extension.vsixmanifest"
+$content = Get-Content $vsixManifestPath
+$xml = New-Object System.Xml.XmlDocument
+
+Write-Host "Reading $vsixManifestPath..."
+$xml.LoadXml($content)
+$namespace = New-Object System.Xml.XmlNamespaceManager($xml.NameTable)
+$namespace.AddNamespace("ns", "http://schemas.microsoft.com/developer/vsx-schema/2011")
+$node = $xml.SelectSingleNode("//ns:PackageManifest/ns:Metadata/ns:Identity", $namespace)
+
+Write-Host "Setting $vsixManifestPath release attributes..."
+$node.SetAttribute("Version", $BuildVersion);
+
+Write-Host "Saving $vsixManifestPath..."
+$xml.Save($vsixManifestPath)
+
 Write-Host "Finish VSIX installer configuration..."
